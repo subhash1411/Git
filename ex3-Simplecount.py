6ab98ce05bb3894b3a482952d2880b9162cbb1fe
subhash1411 <subhashchowdary1411@gmail.com>
2024-07-25 14:13:32 +0530

    3rd Commit : Added ex3 file

---
+++ ex3-Simplecount.py
@@ -0,0 +1,15 @@
+#funtion to print the numbers from 1 to n
+def count_to_n(n):
+    for i in range(1,n+1):
+        print(i, end=" ")
+    print()
+
+#User input Section
+x=int(input("Enter a value of n b/w 10 to 20 :"))
+
+for i in range(1,x):
+    count_to_n(i)
+
+
+#--------------------------------------------------------
+print("Added this file and code, for 3d commit")
