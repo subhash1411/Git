44e64d43b8de4ad996e4077322c9f39bc46796e0
subhash1411 <subhashchowdary1411@gmail.com>
2024-07-25 14:00:32 +0530

    2nd Commit: Modified ex1 and ex2 python files

---
+++ ex1-forloop.py
@@ -0,0 +1,5 @@
+print("printing 1 to 100 numbers using for loop")
+for  i in range(1,101):
+    print(i,end=" ")
+
+print("Program execution is successful")
---
+++ ex2-dfunction.py
@@ -0,0 +1,8 @@
+def double(n):
+    return 2 * n
+print("To call this function using its name and assign the value to the varible, and pass the valuse using varible in the function calling. Than value is doubled and return to main program")
+#a,x=3, double(a)
+print("program Ran Successfully")
+
+#------------------------------------------------------------
+print("first modification for 2nd commit")
